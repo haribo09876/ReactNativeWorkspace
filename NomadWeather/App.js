bd4df6d1553e64865ba8a141108cebf36ff6bae7
haribo09876 <haribo09876@gmail.com>
2024-03-21 00:55:00 +0900

    20240321
    by KYW

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -9,14 +9,20 @@ import {
   PermissionsAndroid,
 } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
+import axios from 'axios';
 
 export default function App() {
   const [location, setLocation] = useState(true);
+  // const [data, setData] = useState();
+  const [city, setCity] = useState(true);
+  // const [temperature, setTemperature] = useState(true);
+  // const [weather, setWeather] = useState(true);
+  // const [time, setTime] = useState(true);
   const weatherApiKey = '174580b1f4ee4ec1e406e56c83717aed';
   const url = `api.openweathermap.org/data/2.5/forecast?lat=${location.latitude}&lon=${location.longitude}&appid=${weatherApiKey}`;
 
+  // 위치 업데이트 설정
   useEffect(() => {
-    // 위치 업데이트 설정
     if (Platform.OS === 'android') {
       PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
@@ -24,8 +30,8 @@ export default function App() {
     }
   }, []);
 
+  // 위치 업데이트 설정
   useEffect(() => {
-    // 위치 업데이트 설정
     const watchId = Geolocation.watchPosition(
       position => {
         const {latitude, longitude} = position.coords;
@@ -48,11 +54,23 @@ export default function App() {
     };
   }, []);
 
+  useEffect(() => {
+    const getCity = axios
+      .get(url)
+      // data 안에 들어가면 바로 활용 가능! json 변환 필요 없음
+      .then(response => {
+        setCity(response.data.city.name);
+      });
+  });
+
+  console.log(city);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
         <Text>{url}</Text>
+        <Text>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
